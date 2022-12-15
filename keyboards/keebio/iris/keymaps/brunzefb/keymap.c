5c794829bd6c834daa4e40a002dd39b346660d22
Friedrich Brunzema <Friedrich.Brunzema@sciex.com>
2022-12-14 19:42:47 -0500

    fixed quote

--- keyboards/keebio/iris/keymaps/brunzefb/keymap.c
+++ keyboards/keebio/iris/keymaps/brunzefb/keymap.c
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                     ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐   ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_RALT,     KC_RASE,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLASH,KC_OCDQUO,
+     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_RALT,     KC_RASE,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLASH,KC_QUOT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘   └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LCTL, KC_LALT, KC_LGUI,              KC_LOWR, KC_SPACE,KC_ADJU
                                 // └────────┴────────┴────────┘            └────────┴────────┴────────┘
