89670a07b63f1068e860dfbcb06351eee4d5473b
Friedrich Brunzema <Friedrich.Brunzema@sciex.com>
2023-04-04 10:44:34 -0400

    formatting

--- keyboards/keebio/iris/keymaps/brunzefb/keymap.c
+++ keyboards/keebio/iris/keymaps/brunzefb/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐   ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_RALT,     KC_RASE,  KC_K,   KC_H,    KC_COMM, KC_DOT,  KC_SLASH,KC_QUOT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘   └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LCTL, KC_LALT, KC_LGUI,              KC_LOWR,   KC_SPACE,KC_ADJU
+                                    KC_LCTL, KC_LALT, KC_LGUI,              KC_LOWR, KC_SPACE, KC_ADJU
                                 // └────────┴────────┴────────┘            └────────┴────────┴────────┘
   ),
 
