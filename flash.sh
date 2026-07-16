0091d1356eb8cde31a04ef270817e3aca8a6438c
friedrich <friedrich.brunzema@sciex.com>
2026-07-16 14:15:12 -0400

    secrets

--- flash.sh
+++ flash.sh
@@ -1,27 +1,48 @@
-#/bin/bash
-linux_path=/home/friedrich/git/opensource/qmk_firmware/usbmount
-lsblk
-read -r -p "Is enter dev: " device
+#!/bin/bash
+mac_path="/Volumes/RPI-RP2"
 
 for i in {1..2}; do
-  echo "Get ready to put keyboard side in flash mode. Press any key to continue."
-  read -n 1 -s -r -p ""
-  echo "Need Flash mode in 10 seconds"
-  # user needs to put keyboard in flash mode
-  # it will take the user a few seconds to do this
-  # and a further few seconds for the system to
-  # detect the new mass stoarge device (from the keyboard)
+  figlet "Flash #$i"
+  echo "Get ready to put keyboard side #$i in flash mode..."
+  echo "You have 10 seconds to put the keyboard in bootloader mode"
   sleep 10
-  mkdir -p usbmount
-  # we have to mount the keyboard mass storage device
-  # explicitly, as it is not mounted automatically
-  sudo mount $device $linux_path
-  # install keyboard firmware by copying it to the mounted drive of the keyboard
-  sudo cp .build/keebio_iris_rev8_brunzefb-v8.uf2 $linux_path/
+  
+  # Wait for the device to appear and mount it
+  echo "Waiting for RPI-RP2 device..."
+  for attempt in {1..10}; do
+    # Find the RPI-RP2 disk
+    DISK=$(diskutil list | grep "RPI-RP2" | awk '{print $NF}')
+    if [ -n "$DISK" ]; then
+      echo "Found RPI-RP2 at $DISK"
+      # Mount it if not already mounted
+      if [ ! -d "$mac_path" ]; then
+        diskutil mount $DISK
+        sleep 1
+      fi
+      break
+    fi
+    echo "Attempt $attempt: Device not found, waiting..."
+    sleep 1
+  done
+  
+  # Check if mounted
+  if [ ! -d "$mac_path" ]; then
+    figlet "Mount Error"
+    echo "Error: Could not find or mount RPI-RP2 device"
+    exit 1
+  fi
+  
+  # Copy firmware
+  cp .build/keebio_iris_rev8_brunzefb-v8.uf2 $mac_path/
   if [ $? -ne 0 ]; then
     figlet "Copy Error"
     exit 1
   fi
-  echo "[Ok] Firmware #$i!"
+  
+  echo "[Ok] Firmware #$i flashed!"
+  echo "Waiting for keyboard to reboot..."
+  sleep 3
 done
+
+figlet "Done"
 
